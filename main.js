7daca2efe6c4ed11f1b532a1453cefe240679871
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-25 12:27:48 -0500

    working on form validation

--- main.js
+++ main.js
@@ -6,11 +6,42 @@
 
 //program will be similar to a Todo list application
 //create an array to hold the images with their captions
-//push the new items into the array.  
+//push the new items into the array when Add Image is clicked
+//then post to server
 
+var imagesArray = [];
 
+//1. Select the element that you want to listen for events on
+//2. Add the event listener
+//3. Create a function to run when that event happens.
 
+var form = $('.addImage');
 
+//when form is submitted run the function
+form.on('submit', function(e) {
 
+	//prevent the default behavior of form being sumbitted 
+	e.preventDefault(); 
+
+	var URL = $('.imageURL').val();
+	var caption = $('.imageCaption').val();
+
+	console.log(URL);
+	console.log(caption);
+
+	//check that prefix of url is correct
+	if ((URL.substring() !== 'http://') || (URL.substring !== 'https://')) {
+		alert('The URL should start with http:// or https://')
+		return;  
+	}
+
+	else if {
+		(caption === '') {
+			console.log('Caption cannot be blank');
+			return;  
+		}
+	}
+
+})
 
 
