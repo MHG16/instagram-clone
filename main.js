54bc5ce0ef9338168ac5a5cbd0e87ade58bdf273
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-25 11:38:08 -0500

    created HTML form

---
+++ main.js
@@ -0,0 +1,16 @@
+// Using the tools you've learned in class, create a responsive image board that allows you 
+// to add images and captions via a url. The images that are added to the image board should be 
+// saved to tiny pizza server, so that when you reload the page, they are not lost. The form to 
+// add an image should properly validate the image URL by at minimum checking for 
+// a http:// or https:// prefix and require a non-empty description.
+
+//program will be similar to a Todo list application
+//create an array to hold the images with their captions
+//push the new items into the array.  
+
+
+
+
+
+
+
