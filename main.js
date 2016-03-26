99419a121d5f85bd556efea81c005f12bbad0904
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-26 13:15:23 -0500

    working on render function to add url, caption to array of objects

--- main.js
+++ main.js
@@ -23,14 +23,46 @@ $(document).ready(function() {
 	});
 });
 
+//render takes the picture(URL) and caption from the user and then puts those in a image object.
+//then image object is prepended to imagesArray 
+//then iterates over the array of image objects each wih a picture(URL) and caption
+//Each object is prepended to the imageHolder DIV.  
+
+//Inputs: array of objects, each with URL and caption
+//Outputs:  None
+
+function render(arr) {
+
+	image = {};
+
+	image.picture = $().val();
+	image.caption = $().val();
+
+	for (var i = 0; i < imagesArray.length; i++) {
+	
+	$('imageHolder') = 
+	
+	}
+	document.querySelector('.txtArea').innerHTML = todoItems; 
+}
+
+
+
+
+
+
+
+
+
+
 //when form is submitted run the function
 form.on('submit', function(e) {
 
 	//prevent the default behavior of form being sumbitted 
 	e.preventDefault(); 
 
-	var enteredURL = $('.imageURL').val();
-	var enteredCaption = $('.imageCaption').val();
+	var enteredURL = $('.pictureURL').val();
+	var enteredCaption = $('.pictureCaption').val();
 
 	console.log(enteredURL);
 	console.log(enteredCaption);
@@ -52,9 +84,10 @@ form.on('submit', function(e) {
 	console.log('success!');
 
 	//if input passes validation, add image and caption to the array
+	render(arr);
 
-	imagesArray = imagesArray.push(enteredURL);
-	console.log(imagesArray);
+	
+
 });
 
 
